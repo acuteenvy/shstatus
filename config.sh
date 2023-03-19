4d509637ca3faec28b1ff949f32886081abd194d
acuteenvy <126529524+acuteenvy@users.noreply.github.com>
2023-03-19 22:09:58 +0100

    Initial commit

---
+++ config.sh
@@ -0,0 +1,14 @@
+# shellcheck shell=bash
+# shellcheck disable=2034
+
+interval=1
+
+kernel="$(uname -sr)"
+update() {
+    block       fab387  "$kernel"
+    loadavg     f38ba8  "LOADAVG: %load1 / %load5 / %load15"
+    mem         74c7ec  "MEM: %memavail SWAP: %swapavail"
+    disk        f2c2e7  "DISK: %avail" "/"
+    pwvol       cba6f7  "VOL: %vol" "@DEFAULT_SINK@"
+    datetime    a6e3a1  "%date" "%c"
+}

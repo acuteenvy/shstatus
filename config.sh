80e31ed139bc89d84f8e32c8c2fe6777ea46e048
acuteenvy <126529524+acuteenvy@users.noreply.github.com>
2023-03-20 00:48:58 +0100

    Add help, handle unmounted disks

--- config.sh
+++ config.sh
@@ -8,7 +8,7 @@ update() {
     block       fab387  "$kernel"
     loadavg     f38ba8  "LOADAVG: %load1 / %load5 / %load15"
     mem         74c7ec  "MEM: %memavail SWAP: %swapavail"
-    disk        f2c2e7  "DISK: %avail" "/"
+    disk        f2c2e7  "DISK: %avail" "DISK: not mounted" "/"
     pwvol       cba6f7  "VOL: %vol" "@DEFAULT_SINK@"
     datetime    a6e3a1  "%date" "%c"
 }

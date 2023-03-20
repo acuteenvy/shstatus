618acfbcc2dfa9907bc2a932042c0c51d0f5e230
acuteenvy <126529524+acuteenvy@users.noreply.github.com>
2023-03-20 22:03:29 +0100

    Add support for more variables from i3/swaybar protocol, change format of config.sh

--- config.sh
+++ config.sh
@@ -4,11 +4,53 @@
 interval=1
 
 kernel="$(uname -sr)"
+kern() {
+    local full_text="$kernel"
+    local color="#fab387"
+
+    block
+}
+
+loadavg() {
+    local full_text="LOADAVG: %load1 / %load5 / %load15"
+    local color="#f38ba8"
+
+    block_loadavg
+}
+
+mem() {
+    local full_text="MEM: %memavail SWAP: %swapavail"
+    local color="#74c7ec"
+
+    block_mem
+}
+
+disk() {
+    local full_text="DISK: %avail"
+    local color="#f2c2e7"
+
+    block_disk "/"
+}
+
+vol() {
+    local full_text="VOL: %vol"
+    local color="#cba6f7"
+
+    block_pwvol "@DEFAULT_SINK@"
+}
+
+datetime() {
+    local full_text="%date"
+    local color="#a6e3a1"
+
+    block_datetime "%c"
+}
+
 update() {
-    block       fab387  "$kernel"
-    loadavg     f38ba8  "LOADAVG: %load1 / %load5 / %load15"
-    mem         74c7ec  "MEM: %memavail SWAP: %swapavail"
-    disk        f2c2e7  "DISK: %avail" "DISK: not mounted" "/"
-    pwvol       cba6f7  "VOL: %vol" "@DEFAULT_SINK@"
-    datetime    a6e3a1  "%date" "%c"
+    kern
+    loadavg
+    mem
+    disk
+    vol
+    datetime
 }

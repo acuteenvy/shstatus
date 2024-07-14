e7353a5e512db48f5aefc5c884697c75463fb6bc
Lena <126529524+acuteenvy@users.noreply.github.com>
2024-07-14 18:16:03 +0200

    Add uptime and comments to the default config

--- config.sh
+++ config.sh
@@ -20,66 +20,84 @@ interval=1
 # shstatus provides `block_*` functions, which will insert
 # system information into the `full_text` and `short_text` variables.
 # See `man shstatus` for details.
-blocks=(kern backlight cpu loadavg mem disk vol battery datetime)
+blocks=(kern up backlight cpu loadavg mem disk vol battery datetime)
 
 kernel="$(uname -sr)"
+# Displays the kernel name and version.
+# The variable is set outside the function, because the kernel
+# won't change while shstatus is running.
 kern() {
     full_text="$kernel"
     color="#fab387"
 }
 
+# Displays the current uptime in hours and minutes,
+# using a space as the separator, "h" to denote hours, and "min"
+# to denote minutes.
+up() {
+    full_text="up %uptime"
+    block_uptime "h" "min" "DISABLE" " "
+}
+
+# Displays the current screen brightness from
+# /sys/class/backlight/intel_backlight/actual_brightness.
 backlight() {
     full_text="BRIGHTNESS: %percentage"
     color="#f9e2af"
-
     block_backlight "intel_backlight" "actual_brightness"
 }
 
+# Displays the current battery status.
+# BAT: CHR %percentage  - when charging,
+# BAT: DIS %percentage  - when discharging,
+# BAT: FULL %percentage - when full and still charging.
 battery() {
     full_text="BAT: %status %percentage"
     color="#94e2d5"
-
     block_battery "BAT1" "CHR" "DIS" "FULL"
 }
 
+# Displays the load average for the last 1, 5 and 15 minutes.
 loadavg() {
     full_text="LOADAVG: %load1 / %load5 / %load15"
     color="#f38ba8"
-
     block_loadavg
 }
 
+# Displays the CPU usage in the last $interval seconds.
 cpu() {
     full_text="CPU: %usage%"
     color="#f2cdcd"
-
     block_cpu
 }
 
+# Displays how much memory and swap is available,
+# as reported by 'free -h'.
 mem() {
     full_text="MEM: %memavail SWAP: %swapavail"
     color="#74c7ec"
-
     block_mem
 }
 
+# Displays available disk space in the root partition,
+# as reported by 'df -h'.
 disk() {
     full_text="DISK: %avail"
     color="#f2c2e7"
-
     block_disk "/"
 }
 
+# Displays the current volume and whether or not sound is muted,
+# as reported by 'wpctl'.
 vol() {
     full_text="VOL: %vol%mutestatus"
     color="#cba6f7"
-
     block_pwvol "@DEFAULT_SINK@" " [MUTED]"
 }
 
+# Displays the current date and time using the bash builtin 'printf'.
 datetime() {
     full_text="%date"
     color="#a6e3a1"
-
     block_datetime "%c" "printf"
 }
